d70f4b1ed91821b0232359310da662807202e0f3
chethangs310 <chethan.gs310@gmail.com>
2024-10-29 15:56:52 +0530

    1st Java-code on maths operations

---
+++ Test.java
@@ -0,0 +1,18 @@
+public class Test
+{
+    public static void main(String[] args)
+    {
+        int a = 10;
+        int b = 20;
+        int c = 15;
+        int d = 2;
+
+
+        System.out.println( "addition = " +(a + b));
+        System.out.println( "sub = " +(a - b));
+        System.out.println( "mul = " +(a * b));
+        System.out.println( "div = " +(a / b));
+
+
+    }
+}
