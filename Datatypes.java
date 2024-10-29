384e4cbdb153ed6961fdb6c338b5ad8cddd781d2
chethangs310 <chethan.gs310@gmail.com>
2024-10-29 18:11:38 +0530

    all primitive Data Types

---
+++ Datatypes.java
@@ -0,0 +1,24 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        byte byteNum = 10;
+        short shotNum = 1500;
+        int intNum = 25000;
+        long longNum = 15240365L;
+        float FloatNum = 987654.12345f;
+        double DoubleNum = 123456789.123456789;
+        boolean flag = true;
+        char letter = 'K';
+
+        System.out.println("The Output of all DataTypes ");
+        System.out.println("Byte Value is = " + byteNum );
+        System.out.println("Short Value is = " + shotNum );
+        System.out.println("Int Value is = " + intNum );
+        System.out.println("Long Value is = " + longNum );
+        System.out.println("Floating Value is = " + FloatNum );
+        System.out.println("Double Value is = " + DoubleNum);
+        System.out.println("Boolean Value is = " + flag );
+        System.out.println("Character  Value is = " + letter );
+
+
+    }
+}
